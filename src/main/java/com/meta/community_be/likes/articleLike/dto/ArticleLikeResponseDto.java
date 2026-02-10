2c015d5a5f23ae080bd4de1ed0860e1ad807d9ae
wonjae1230 <lwoj3019515@gmail.com>
2026-02-10 10:38:39 +0900

    fix: 중복 클래스 제거

--- src/main/java/com/meta/community_be/likes/articleLike/dto/ArticleLikeResponseDto.java
+++ src/main/java/com/meta/community_be/likes/articleLike/dto/ArticleLikeResponseDto.java
@@ -1,71 +1,22 @@
-package com.meta.community_be.article.dto;
+package com.meta.community_be.likes.articleLike.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.meta.community_be.article.domain.Article;
-import com.meta.community_be.comment.dto.CommentResponseDto;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
-public class ArticleResponseDto {
-    private Long id;
-    private String title;
-    private String contents;
-    private String authorUsername;
-    private String authorNickname;
+@Setter
+@NoArgsConstructor
+public class ArticleLikeResponseDto {
+    private Long articleId;
+    private Long userId;
     private boolean liked;
     private int likesCount;
-    private String boardTitle;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime modifiedAt;
-
-    private List<CommentResponseDto> comments;
-
-    // 게시글 상세 조회를 위해서 만든 DTO
-    public ArticleResponseDto(Article article, int likesCount, boolean liked, List<CommentResponseDto> comments) {
-        this.id = article.getId();
-        this.title = article.getTitle();
-        this.contents = article.getContents();
-        this.createdAt = article.getCreatedAt();
-        this.modifiedAt = article.getModifiedAt();
-
-        if (article.getUser() != null) {
-            this.authorUsername = article.getUser().getUsername();
-            this.authorNickname = article.getUser().getNickname();
-        }
-
-        this.likesCount = likesCount;
+    public ArticleLikeResponseDto(Long articleId, Long userId, boolean liked, int likesCount) {
+        this.articleId = articleId;
+        this.userId = userId;
         this.liked = liked;
-        this.comments = comments;
-    }
-
-    // 게시글 목록 조회를 위해 만든 DTO
-    public ArticleResponseDto(Article article) {
-        this.id = article.getId();
-        this.title = article.getTitle();
-        this.contents = article.getContents();
-        this.createdAt = article.getCreatedAt();
-        this.modifiedAt = article.getModifiedAt();
-
-        if (article.getBoard() != null) {
-            this.boardTitle = article.getBoard().getTitle();
-        }
-
-        if (article.getUser() != null) {
-            this.authorUsername = article.getUser().getUsername();
-            this.authorNickname = article.getUser().getNickname();
-        }
-
-        this.likesCount = 0;
-        this.liked = false;
-
-        this.comments = List.of(); // 목록 조회 시에는 comments는 제외 (N+1 방지)
+        this.likesCount = likesCount;
     }
 }
